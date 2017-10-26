59fb387f580cd7c624c280d9b55a8157e4032de7
Ricenog <erickasngo@gmail.com>
2017-10-25 23:52:13 -0700

    Almost done

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 This is a presentation for Bio on an ever-lasting mythbuster question: are microwave ovens carcinogenic?
 I definetly could have created this using more refined frameworks, but for this project I decided to stick to my roots as its more fun. The reward is ten times better when you get it to work.
 I use html, css, and javascript. I have one file in .json, but it was to force Herou to use harp as my server since the site is 'static'. Heroku only servs dynamic sites and this was a workaround for sites that don't use frameworks such as ruby on rails. 
+The CSS definetly needs some refactoring, but as of now it's permissible. Plus, I'm the only one who is working on this, and will probably not touch it again after today, 10/25.
+I've decided to use jQuery mid way so in the js file, you will see javaSquerypt.
+Common selectors:
+    $("*") - Wildcard: selects every element on the page.
+    $(this) - Current: selects the current element being operated on within a function.
+    $("p") - Tag: selects every instance of the <p> tag.
+    $(".example") - Class: selects every element that has the example class applied to it.
+    $("#example") - Id: selects a single instance of the unique example id.
+    $("[type='text']") - Attribute: selects any element with text applied to the type attribute.
+    $("p:first-of-type") - Pseudo Element: selects the first <p>.
+Common Events:
+
+    click()- Click: executes on a single mouse click.
+    hover() - Hover: executes when the mouse is hovered over an element. mouseenter() and mouseleave() apply only to the mouse entering or leaving an element, respectively.
+    submit() - Submit: executes when a form is submitted.
+    scroll() - Scroll: executes when the screen is scrolled.
+    keydown() - Keydown: executes when you press down on a key on the keyboard.
+jQuery also has built in effects to the functionalities of animations and object manipulations are made available. 
+I should use partials and #includes next time. Here is a quick tutorial for those who are curious(me). https://www.html5rocks.com/en/tutorials/webcomponents/imports/ .
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,77 @@
 //Can use this, or async and defer.
 //<script src=... async/defer></script>
 document.addEventListener("DOMContentLoaded", function(){
-
+	title.innerHTML = "Origin of the Myth";
+	loadOOM();
+});
+$(document).ready(function() {
+    // all custom jQuery will go here
 
 });
+var title = document.getElementById('the_title');
 
 var myImage = document.querySelector("img");
 var myImage2 = document.querySelector("img");
-myImage.onclick = function(){
-var mySrc = myImage.getAttribute('src');
-	if(mySrc==='src/img/MythBusters.jpg'){
+var post_body = document.getElementById('the_post');
+var oom_nav = document.getElementById('oom');
+// oom_nav.onclick = function(){
+// 	oom_nav.innerHTML = "blue";
+// 	post_body.innerHTML="<img src='src/img/oven2.png'/>";
+// 	     	// '<object http-equiv="content-type" type="text/html" charset="utf-8" data="content/origin_of_myth.html" ></object>';
+// };
 
-	} else {
-		alert("This is the other img");
-	}
+$("#oom").on("click", function() {
+	loadOOM();
+ });
+$("#htw").on("click", function() {
+	loadHTW();
+ });
+$("#sof").on("click", function() {
+	loadSOF();
+ });
+$("#con").on("click", function() {
+	loadCON();
+ });
+// $("#htw").click(function(){
+// 	$(this).html("blue ");
+// 	post_body.innerHTML = "blue red";
+// });
+function loadOOM(){
+	var $this = $(this);
+
+	$("#the_post").load("content/origin_of_myth.html");
+	$("#the_title").html("Origin of the Myth");
 }
+function loadHTW(){
+	var $this = $(this);
+	$("#the_post").load("content/how_they_work.html");
+	$("#the_title").html("How They Work");
+}
+function loadSOF(){
+	var $this = $(this);
+	$("#the_post").load("content/support_of_findings.html");
+	$("#the_title").html("Support of Findings");
+}
+function loadCON(){
+	var $this = $(this);
+	$("#the_post").load("content/conclusion.html");
+	$("#the_title").html("Conclusion");	
 
+}
+function changeClickedBackground(ob){
+	ob.css({
+		'background' : '#ccc'
+	});
+}
+$(".navd").click(function(event){
+	// event.target.id.css({
+	// 	'background' : '#ccc'
+	// });
+	var $cur = event.target.id;
+	if($cur.endsWith("s")){
+		$cur = $cur.slice(0, -1);
+	}
+	var $this = $($cur);
+	changeClickedBackground($this);
 
+});
